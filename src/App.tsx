3b353e7701874899658a0e6d9ffe8274c3c3fb8d
Tên GitHub mớiHzmkoito <huydnse180003@fpt.edu.vn>
2026-02-02 15:01:01 +0700

    Add guard for admin/client and fix Header

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,21 @@ import NotFoundPage from "./pages/NotFoundPage.page";
 import AdminAuthRoute from "./router/admin/admin.auth.route";
 import ClientAuthRoute from "./router/client/client.auth.route";
 import { Toaster } from "@/components/ui/sonner";
+import { useAuthStore } from "./stores/auth-store";
+import { useEffect } from "react";
+import LoadingLayout from "./layouts/loading-layout";
 
 function App() {
+  const { hydrate, isInitialized } = useAuthStore();
+
+  useEffect(() => {
+    hydrate();
+  }, [hydrate]);
+
+  if (!isInitialized) {
+    return <LoadingLayout />;
+  }
+
   return (
     <>
       <BrowserRouter>
--- src/layouts/admin-layout/admin-layout.tsx
+++ src/layouts/admin-layout/admin-layout.tsx
@@ -33,4 +33,5 @@ const AdminLayout = () => {
     </div>
   );
 };
+
 export default AdminLayout;
