5797275d9feb353dfc8d9850da4c33a7efd97632
nhathuyck1 <dangnhathuy61@gmail.com>
2026-03-15 21:39:08 +0700

    Fixing button for im/export excel

--- src/layouts/admin-layout/admin-layout.tsx
+++ src/layouts/admin-layout/admin-layout.tsx
@@ -13,7 +13,7 @@ const AdminLayout = () => {
   return (
     <div className="flex h-screen overflow-hidden bg-[#FAF8F5]">
       {isSwitchingRole && (
-        <LoadingLayout forceVisible message="Switching role" />
+        <LoadingLayout forceVisible message="Switching" />
       )}
       <AdminSidebar collapsed={sidebarCollapsed} />
 
