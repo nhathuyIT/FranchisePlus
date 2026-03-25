0b439f699c7f2f1e2fed5d3739c4f5093b789856
DiepKhai <aicungduoc49@gmail.com>
2026-03-25 12:45:20 +0700

    Fix route

--- src/router/route.const.ts
+++ src/router/route.const.ts
@@ -66,7 +66,6 @@ export const ROUTER_URL = {
     USER_FRANCHISE_ROLES: "user-franchise-roles",
     CUSTOMERS: "customers",
     CART: "cart",
-    CART_CHECKOUT: "cart/:cartId/checkout",
-    ORDERS: "orders",
+    CART_CHECKOUT: "cart/:cartId/checkout"
   },
 };
