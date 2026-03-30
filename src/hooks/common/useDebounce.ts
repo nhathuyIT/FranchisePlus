7ab67d9d70415adf1d15f1fcfccf8efab6fb770a
Kietlatoi <toilakiet24@gmail.com>
2026-03-30 09:12:46 +0700

    [30032026] [KietDM] feat api staff list

--- src/hooks/common/useDebounce.ts
+++ src/hooks/common/useDebounce.ts
@@ -8,7 +8,7 @@ export function useDebounce<T>(value: T, delay = 2000, digit: string): T {
   const [debounced, setDebounced] = useState<T>(value);
 
   useEffect(() => {
-    // if (digit.length < 3 && digit.length > 0) return;
+    if (digit.length < 3 && digit.length > 0) return;
 
     const timer = setTimeout(() => setDebounced(value), delay);
     return () => clearTimeout(timer);
